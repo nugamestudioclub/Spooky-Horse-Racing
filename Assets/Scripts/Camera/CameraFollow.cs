48270ae97a34d69376f065ae58a1abd15de35c8d
firez2469 <hampel.m@northeastern.edu>
2022-07-08 12:17:48 -0400

    applied rotation based on velocity to horse

---
+++ Assets/Scripts/Camera/CameraFollow.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour
+{
+    [SerializeField]
+    private Transform target;
+
+    [SerializeField]
+    private float followStrength;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        Vector3 newTarg = new Vector3(target.transform.position.x,target.transform.position.y,transform.position.z);
+        transform.position = Vector3.Lerp(newTarg, transform.position, Time.deltaTime);
+    }
+}
---
+++ Assets/Scripts/HorseController.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HorseController : MonoBehaviour
+{
+    [SerializeField]
+    private Rigidbody2D target;
+
+    private Vector2 prevPos;
+    
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        prevPos = target.transform.position;   
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        transform.position = target.transform.position;
+        
+    }
+    private void FixedUpdate()
+    {
+
+        Vector2 delta = (Vector2)target.transform.position - prevPos;
+        float angle = (Mathf.Atan2(delta.y, delta.x)/(Mathf.PI*2))*360;
+        print(angle);
+        transform.localEulerAngles = new Vector3(transform.localEulerAngles.x,
+            transform.localEulerAngles.y,
+            angle);
+        prevPos = target.transform.position;
+    }
+}
