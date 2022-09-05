49cb5fd11e2dfa3b82222d983e81bb480837d3b7
BBBrassil <56983102+BBBrassil@users.noreply.github.com>
2022-09-05 14:26:37 -0400

    set up spawn points

--- Assets/Scripts/RollPhysics.cs
+++ Assets/Scripts/RollPhysics.cs
@@ -86,7 +86,6 @@ public class RollPhysics : MonoBehaviour {
 	private void OnCollisionEnter2D(Collision2D collision) {
 		if( collision.gameObject.CompareTag("Ground") ) {
 			IsGrounded = true;
-			print("Hit ground");
 		}
 
 	}
