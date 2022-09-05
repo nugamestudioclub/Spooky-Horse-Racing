a7dbbba0b3decaa83fcab82828dc836a4193bc33
BBBrassil <56983102+BBBrassil@users.noreply.github.com>
2022-09-04 22:26:49 -0400

    changed camera follow to FixedUpdate to eliminate stutter

--- Assets/Scripts/Camera/CameraFollow.cs
+++ Assets/Scripts/Camera/CameraFollow.cs
@@ -2,39 +2,26 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class CameraFollow : MonoBehaviour
-{
-    [SerializeField]
-    private Transform target;
+public class CameraFollow : MonoBehaviour {
+	[SerializeField]
+	private Transform target;
 
-    public Transform Target
-    {
-        get => target;
-        set => target = value;
-    }
+	public Transform Target {
+		get => target;
+		set => target = value;
+	}
 
 
-    [SerializeField]
-    private Camera myCamera;
+	[SerializeField]
+	private Camera myCamera;
 
-    public Camera Camera => myCamera;
+	public Camera Camera => myCamera;
 
-    [SerializeField]
-    private float followStrength;
+	[SerializeField]
+	private float followStrength;
 
-
-
-
-    // Update is called once per frame
-    void FixedUpdate()
-    {
-        if (target != null)
-        {
-            Vector3 newTarg = new Vector3(target.transform.position.x, target.transform.position.y, transform.position.z);
-            transform.position = Vector3.Lerp(newTarg, transform.position, Time.deltaTime);
-        }
-
-    }
-
-
+	void LateUpdate() {
+		if( target != null )
+			myCamera.transform.position = target.position + Vector3.back;
+	}
 }
