37799c2b574054f292eabfe98e0a0c9d465cea78
BBBrassil <56983102+BBBrassil@users.noreply.github.com>
2022-09-05 16:55:58 -0400

    added mod arithmetic to SpriteSheet indexer

--- Assets/Scripts/SpriteSheet.cs
+++ Assets/Scripts/SpriteSheet.cs
@@ -10,7 +10,7 @@ public class SpriteSheet : ScriptableObject, IReadOnlyCollection<Sprite>
     [SerializeField]
     private Sprite[] sprites;
 
-    public Sprite this[int index] => sprites[index];
+    public Sprite this[int index] => sprites[index % Count];
 
     public int Count => sprites.Length;
 
